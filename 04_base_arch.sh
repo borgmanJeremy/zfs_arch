fa86559114f05cf111dd896249242fdd0152a026
Jeremy Borgman <borgman.jeremy@pm.me>
2020-06-04 10:22:43 -0500

    working

--- 04_base_arch.sh
+++ 04_base_arch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 pacstrap /mnt base linux linux-firmware
-cp -R ../ /mnt/tmp
+cp -R ../ /mnt/mnt/tmp
 arch-chroot /mnt
+
+echo "[archzfs]
+Server = http://archzfs.com/$repo/x86_64
+Server = http://mirror.sum7.eu/archlinux/archzfs/$repo/x86_64
+Server = https://mirror.biocrafting.net/archlinux/archzfs/$repo/x86_64
+
+[archzfs-kernels]
+Server = http://end.re/$repo/" >> /mnt/etc/pacman.conf
--- 05_after_chroot.sh
+++ 05_after_chroot.sh
@@ -3,3 +3,4 @@
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
 local-gen
+pacman -S zfs-linux
